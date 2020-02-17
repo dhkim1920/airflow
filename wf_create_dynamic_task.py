0334c72eb597af42eb3188457447838526e33955
dhkim1920 <dhkim1920@gmail.com>
2020-02-18 08:29:43 +0900

    example create dynamic task

---
+++ wf_create_dynamic_task.py
@@ -0,0 +1,37 @@
+from datetime import timedelta
+
+from airflow.operators.bash_operator import BashOperator
+from airflow.models import DAG
+
+
+def create_bash_task(task_index):
+    return BashOperator(
+        task_id='bash_task' + str(task_index),
+        bash_command='echo 1',
+        dag=dag
+    )
+
+
+default_dag_args = {
+    'owner': 'test',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'email': ['test@test.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
+
+dag = DAG('wf_create_dynamic_task',
+          start_date=datetime(2020, 2, 18),
+          default_args=default_dag_args,
+          schedule_interval=None,
+          catch_up=False,
+          )
+
+run_tasks = []
+for index in range(0, 3):
+    run_tasks.append(create_bash_task(index))
+
+for run_task_index in range(len(run_tasks)):
+    if run_task_index not in [0]:
+        run_tasks[run_task_index - 1] >> run_tasks[run_task_index]
